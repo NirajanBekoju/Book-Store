c3347af1de462d6ccd37379c83155b5e972c9b16
Nirajan Bekoju <nirajan.bekoju@gmail.com>
2020-11-22 08:17:33 +0545

    Project Build

---
+++ bookstore/settings.py
@@ -0,0 +1,137 @@
+from pathlib import Path
+import os
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '!qj)d!^ctk==v$ez-b&d^kwxc1a=ym)9+*0@!2_^h0j82v)9x0'
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+
+# Application definition
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'accounts',
+    'book',
+    'inquiry',
+    'contact'
+]
+
+MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'bookstore.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'build')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'bookstore.wsgi.application'
+
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+
+# Password validation
+# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
+
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.1/howto/static-files/
+AUTH_USER_MODEL = 'accounts.Account'
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'build/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny'
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ],
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 40
+}
+
+CORS_ORIGIN_ALLOW_ALL = True
+FILE_UPLOAD_PERMISSIONS = 0o640
---
+++ src/components/BookDetail/BuyModal.js
@@ -0,0 +1,124 @@
+import React, { useState } from 'react'
+import { Modal } from "react-bootstrap"
+import {paymentMethods} from '../Data'
+import axios from 'axios'
+import { setAlert } from '../../redux/alert/AlertAction'
+import { connect } from 'react-redux';
+import { getCookie } from "../../HelperFunctions";
+
+
+function BuyModal({ show, handleClose, id, title, setAlert }) {
+  const csrftoken = getCookie("csrftoken");
+
+  const book_id = id
+    const [formData, setFormData] = useState({
+        name: "",
+        email: "",
+        message: "",
+        payment_method: "Cash"
+    })
+
+    const {name, email, message, payment_method} = formData
+
+    const handleChange = (e) => {
+        setFormData((prevFormData) => {
+            return {...prevFormData, [e.target.name]:e.target.value}
+        })
+    }
+
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      axios.defaults.headers = {
+        "Content-Type": "application/json",
+        "X-CSRFToken": csrftoken,
+      };
+
+      axios.post("http://127.0.0.1:8000/api/inquiry/", {
+        name,
+        email,
+        book_id,
+        message,
+        payment_method
+      }).then(res => {
+        setAlert(`${res.data.success}`, "success")
+      })
+      .catch(err => {
+        console.log(err);
+      })
+      window.scrollTo(0,0);
+      handleClose();
+    }
+
+    return (
+        <Modal show={show} onHide={handleClose} style={{ zIndex: "999999999" }}>
+          <Modal.Header closeButton>
+            <Modal.Title>Buy "{title}"</Modal.Title>
+          </Modal.Header>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <Modal.Body>
+              <div className="form-group">
+                <label htmlFor="name">Name</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Name"
+                  name="name"
+                  id="name"
+                  value={name}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Email"
+                  name="email"
+                  id="email"
+                  value={email}
+                  onChange={(e) => handleChange(e)}
+                />
+              </div>
+              <div className="form-group">
+                <label htmlFor="message">Message</label>
+                <textarea
+                  className="form-control"
+                  placeholder="Enter your Message"
+                  name="message"
+                  id="message"
+                  value={message}
+                  onChange={(e) => handleChange(e)}
+                ></textarea>
+              </div>
+              <div className="form-group">
+                <label htmlFor="payment">Payment Method</label>
+                <select
+                  name="payment_method"
+                  id="payment_method"
+                  value={payment_method}
+                  onChange={(e) => handleChange(e)}
+                >
+                  {paymentMethods.map((method) => {
+                    return <option value={method} key={method}>{method}</option>;
+                  })}
+                </select>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <button className="btn btn-success" type="submit">
+                Submit
+              </button>
+            </Modal.Footer>
+          </form>
+        </Modal>
+    );
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setAlert: (msg, alertType) => dispatch(setAlert(msg, alertType)),
+  };
+};
+
+export default connect(null, mapDispatchToProps)(BuyModal)
